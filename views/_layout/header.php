f2751e07946ac9a5c48e00bfef42a699378937dd
Rasmonikos <radoslav.iliefff@gmail.com>
2017-03-17 22:48:46 +0200

    Setting a greeting message based on user's time

--- views/_layout/header.php
+++ views/_layout/header.php
@@ -20,27 +20,49 @@
 
 <body>
 <header>
-    <a href="<?=APP_ROOT?>"><img src="<?=APP_ROOT?>/content/images/draigoch_logo.png"></a>
-    <a href="<?=APP_ROOT?>/">Home</a>
+    <a href="<?=APP_ROOT?>/"><img class="logo-style" src="<?=APP_ROOT?>/content/images/site-logo.png"></a>
+    <a class="hover-effects" href="<?=APP_ROOT?>/">Home</a>
     <?php if ($this->isLoggedIn) : ?>
-        <a href="<?=APP_ROOT?>/posts">Posts</a>
-        <a href="<?=APP_ROOT?>/comments">Comments</a>
-        <a href="<?=APP_ROOT?>/posts/create">Create Post</a>
-        <a href="<?=APP_ROOT?>/categories">Categories</a>
-        <a href="<?=APP_ROOT?>/users">Users</a>
+        <a class="hover-effects" href="<?=APP_ROOT?>/posts">Posts</a>
+        <a class="hover-effects" href="<?=APP_ROOT?>/comments">Comments</a>
+        <a class="hover-effects" href="<?=APP_ROOT?>/posts/create">Create Post</a>
+        <a class="hover-effects" href="<?=APP_ROOT?>/categories">Categories</a>
+        <a class="hover-effects" href="<?=APP_ROOT?>/users">Users</a>
     <?php else: ?>
-        <a href="<?=APP_ROOT?>/users/login">Login</a>
-        <a href="<?=APP_ROOT?>/users/register">Register</a>
+        <a class="hover-effects" href="<?=APP_ROOT?>/users/login">Login</a>
+        <a class="hover-effects" href="<?=APP_ROOT?>/users/register">Register</a>
     <?php endif; ?>
 
     <?php if ($this->isLoggedIn) : ?>
         <div id="logged-in-info">
-            <span>Hello, <b><?=htmlspecialchars($_SESSION['username'])?></b></span>
+            <span>
+
+            <?php
+            date_default_timezone_set('Europe/Sofia');
+            $time = date("H");
+            if ($time < "12") {
+                echo "Good Мorning,";
+            } else
+            if ($time >= "12" && $time < "17") {
+                echo "Good Afternoon,";
+            } else
+            if ($time >= "17" && $time < "19") {
+                echo "Good Evening,";
+            } else
+            if ($time >= "19") {
+                echo "Good Night,";
+            }
+            ?> 
+
+            <b><?=htmlspecialchars($_SESSION['username']) .'!'?></b></span>
             <form method="post" action="<?=APP_ROOT?>/users/logout">
-                <input type="submit" value="Logout"/>
+                <input class="btn" type="submit" value="Logout"/>
             </form>
         </div>
     <?php endif; ?>
 </header>
 
 <?php require_once('show-notify-messages.php'); ?>
+
+
+
