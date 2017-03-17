5aa19b1a93722edc726f17fab981479d43378bf8
fearr1 <fearr1@abv.bg>
2017-03-17 21:29:27 +0200

    updated footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+    <footer>
+        (c) Draigoch Team, 2017
+    </footer>
+
+    <?php require_once('fill-posted-fields.php'); ?>
+
+    <?php require_once('show-validation-errors.php'); ?>
+
+</body>
+
+</html>
